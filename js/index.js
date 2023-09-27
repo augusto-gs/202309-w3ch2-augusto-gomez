9b482c6e1e1fd3d82170a5dd5137d2aa372156a2
augusto-gs <agomezsantos95@gmail.com>
2023-09-27 17:33:14 +0200

    Add function getMatchResult

--- js/index.js
+++ js/index.js
@@ -35,3 +35,15 @@ const getRandomCard = () => {
   const randomNumber = Math.floor(Math.random() * getDeckOfCards().length);
   return getDeckOfCards()[randomNumber];
 };
+
+const getMatchResult = () => {
+  const invisibleCard = getRandomCard();
+  const userCard = getRandomCard();
+  const cardComparation = [invisibleCard, userCard];
+
+  if (invisibleCard.cardValues < userCard.cardValues) {
+    return cardComparation.push("Greater");
+  } else if (invisibleCard.cardValues > userCard.cardValues) {
+    return cardComparation.push("Smaller");
+  }
+};
